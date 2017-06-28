d99dbecdec3b92c8d5cfea612ec3b5731104d039
zCodeLAT <esteban.martin7@gmail.com>
2017-06-28 12:44:16 -0300

    Code commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express =   require("express");
 var multer  =   require('multer');
 var Ableton = require('ableton');
+var cheerio = require('cheerio');
 var ableton = new Ableton('./8BIT_composicion.als');
 
 var app         =   express();
@@ -35,7 +36,37 @@ app.get('/',function(req,res){
       res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/yourgraphic',  function (req, res, next) {
+app.post('/yourgraphic', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      // An error occurred when uploading
+      return res.end("Error uploading file.");
+    }
+    //res.end("File is uploaded");
+    console.log(getAlsData());
+    res.send("taka taka");
+    // Everything went fine
+  })
+})
+
+function getAlsData() {
+  ableton.read(function(error, $) {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    // `$` is the Cheerio root object.
+
+      $('miditrack').map(function(i, el) {
+          // this === el
+          return $(this).attr('id');
+        }).get().join(', ')
+
+  }
+});
+}
+/*
+app.post('/yourgraphic', function (req, res, next) {
   upload(req,res,function(err) {
       if(err) {
           return res.end("Error uploading file.");
@@ -47,33 +78,30 @@ app.post('/yourgraphic',  function (req, res, next) {
         //     return res.end("Error uploading file.");
         // }
     // });
-    ableton.read(function(error, $) {
-                  if (error) {
-                    console.error(error);
-                  }
-                  else {
-                    //var $ = cheerio.load('/8BIT_')
-                    // `$` is the Cheerio root object.
-                    //res.send( $.root().html() );
-                    console.log(
+      ableton.read(function(error, $) {
+            if (error) {
+              console.error(error);
+            }
+            else {
+              //var $ = cheerio.load('/8BIT_')
+              // `$` is the Cheerio root object.
+              //res.send( $.root().html() );
+              console.log(
 
-                      $('midikey').map(function(i, el) {
-                          // this === el
-                          return $(this).attr('value');
-                        }).get().join(', ')
+                $('midikey').map(function(i, el) {
+                    // this === el
+                    return $(this).attr('value');
+                  }).get().join(', ')
 
-                    );
-                    res.send('taca taca'
-                    );
-                  }
-
-
-                })
-              })
+              );
+              res.send('taca taca');
+            }
+        })
+  })
 
         res.end("File is uploaded");
 });
-
+*/
 app.listen(3000,function(){
     console.log("Working on port 3000");
 });
